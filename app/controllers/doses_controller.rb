b0d9641d2778f7564cbaeea17316ed87dda852d0
Julia Mathias <juliamss@gmail.com>
2020-10-17 15:58:54 -0300

    Improved Show view

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,14 @@
 class DosesController < ApplicationController
-  before_action :set_cocktail, only: [:new, :create]
-  def new
-    @dose = Dose.new
-  end
+  before_action :set_cocktail, only: [:create]
+  # def new
+  #   @dose = Dose.new
+  # end
 
   def create
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to_cocktail_path(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render "cocktails/show"
     end
@@ -17,7 +17,7 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to_cocktail_path(@dose.cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'cocktails/index'
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:create]
+    resources :doses, only: [:create, :new]
   end
   resources :doses, only: [:destroy]
 end
